5dc0db1aaf076bb7f1701d657ce500f448a77bf2
Gustavo <gustavo.aranda.sccp@gmail.com>
2026-08-23 22:27:00 -0300

    feat: deploy do protótipo de frontend

--- src/glaciar.Infrastructure/obj/Debug/net10.0/glaciar.Infrastructure.AssemblyInfo.cs
+++ src/glaciar.Infrastructure/obj/Debug/net10.0/glaciar.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("glaciar.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf871d3a799a3c201d972297ec26b7567fcbc8b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("glaciar.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("glaciar.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
