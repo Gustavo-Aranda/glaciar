8b9e456d76dd8416bc8a3cf06ea49fe67fbd1519
Gustavo <gustavo.aranda.sccp@gmail.com>
2026-09-16 15:30:44 -0300

    feat: inclui as chamadas para o CRUD de Clientes na camada de acesso

--- src/glaciar.API/Program.cs
+++ src/glaciar.API/Program.cs
@@ -1,41 +1,56 @@
+using Microsoft.EntityFrameworkCore;
+using glaciar.Infrastructure.Data;
+using glaciar.Domain.Interfaces.Repositories;
+using glaciar.Infrastructure.Repositories;
+using glaciar.Application.Services.Clientes;
+using glaciar.Infrastructure.Security;
+using glaciar.Application.Interfaces.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// ==========================================
+// CONFIGURAÇÃO DO SUPABASE
+// ==========================================
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// ==========================================
+// INJEÇÃO DE DEPENDÊNCIAS
+// ==========================================
+// Infrastructure -> Domain:
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+// Application -> Infrastructure: Injetando o Hasher
+builder.Services.AddScoped<IPasswordHasher, PasswordHasher>(); 
+
+// API -> Application: Injetando o Serviço
+builder.Services.AddScoped<UsuarioService>();
+
+// ==========================================
+// CONFIGURAÇÃO DE CORS (LIBERAR FRONTEND)
+// ==========================================
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirFrontend", policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:5205")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+// Ativa o CORS configurado acima ANTES da autorização
+app.UseCors("PermitirFrontend");
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
