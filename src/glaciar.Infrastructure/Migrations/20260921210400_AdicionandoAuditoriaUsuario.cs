7b263a7244c3c7a3e1338a9de215b63d19d6ebd4
Gustavo <gustavo.aranda.sccp@gmail.com>
2026-09-21 18:38:00 -0300

    feat: adiciona auditoria de usuários com campos Ativo e DataCadastro

---
+++ src/glaciar.Infrastructure/Migrations/20260921210400_AdicionandoAuditoriaUsuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace glaciar.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoAuditoriaUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "UsuariosEnderecos",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TipoUsuario",
+                table: "Usuarios",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(3)",
+                oldMaxLength: 3);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Ativo",
+                table: "Usuarios",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCadastro",
+                table: "Usuarios",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "UsuariosEnderecos");
+
+            migrationBuilder.DropColumn(
+                name: "Ativo",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "DataCadastro",
+                table: "Usuarios");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TipoUsuario",
+                table: "Usuarios",
+                type: "character varying(3)",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
